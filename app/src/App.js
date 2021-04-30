b46203d373d3e6b420ddd768030e124cbb925b78
George Song <george@monozuku.com>
2021-04-30 09:11:54 -0700

    feat: add integration with Google Calendar

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,7 @@
 import * as React from "react";
 
+import gcal from "react-google-calendar-api";
+
 import * as apiClient from "./apiClient";
 
 const App = () => {
@@ -11,8 +13,19 @@ const App = () => {
     loadTasks();
   }, []);
 
+  const [isAuthenticated, setIsAuthenticated] = React.useState(gcal.sign);
+
+  React.useEffect(() => {
+    gcal.onLoad(() => {
+      setIsAuthenticated(gcal.gapi.auth2.getAuthInstance().isSignedIn.get());
+      gcal.listenSign((sign) => setIsAuthenticated(sign));
+    });
+  }, []);
+
   return (
     <main className="App">
+      <Login {...{ isAuthenticated }} />
+      {isAuthenticated ? <Events /> : null}
       <TaskList tasks={tasks} />
       <AddTask loadTasks={loadTasks} />
     </main>
@@ -49,6 +62,31 @@ const AddTask = ({ loadTasks }) => {
       </label>
       <button disabled={!canAdd}>Add</button>
     </form>
+  );
+};
+
+const Login = ({ isAuthenticated }) =>
+  isAuthenticated ? (
+    <button onClick={gcal.handleSignoutClick}>Log out</button>
+  ) : (
+    <button onClick={gcal.handleAuthClick}>Log in</button>
+  );
+
+const Events = () => {
+  const [events, setEvents] = React.useState([]);
+
+  React.useEffect(() => {
+    gcal
+      .listUpcomingEvents(10)
+      .then(({ result: { items } }) => setEvents(items));
+  }, []);
+
+  return events.length === 0 ? null : (
+    <ul>
+      {events.map((event) => (
+        <li key={event.id}>{event.summary}</li>
+      ))}
+    </ul>
   );
 };
 
